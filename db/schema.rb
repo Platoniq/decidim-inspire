7c38060609f69d0ac4847b7912100e45e50321d9
Harrison Hassig <harrisonhassig@gmail.com>
2026-05-13 16:42:16 +0200

    Upgrade steps to 0.30.4

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2026_04_24_044836) do
+ActiveRecord::Schema[7.0].define(version: 2026_05_13_144106) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "ltree"
   enable_extension "pg_trgm"
@@ -1512,7 +1512,8 @@ ActiveRecord::Schema[7.0].define(version: 2026_04_24_044836) do
     t.index ["privatable_to_type", "privatable_to_id"], name: "space_privatable_to_privatable_id"
   end
 
-  create_table "decidim_private_exports", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+  create_table "decidim_private_exports", force: :cascade do |t|
+    t.uuid "uuid", null: false
     t.string "export_type", null: false
     t.string "attached_to_type"
     t.integer "attached_to_id"
@@ -1523,6 +1524,7 @@ ActiveRecord::Schema[7.0].define(version: 2026_04_24_044836) do
     t.jsonb "metadata", default: {}
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["uuid"], name: "index_decidim_private_exports_on_uuid", unique: true
   end
 
   create_table "decidim_proposals_collaborative_draft_collaborator_requests", force: :cascade do |t|
